f74bd81651de99de03ffc22609ae027ec588fd19
tischulte <tim.schulte@comcast.net>
2016-09-27 14:05:10 -0500

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,9 +1,5 @@
+class user {
 user {'fundamentals':
   ensure => present,
   }
-  node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  notify { "Hello Puppet Master": }
 }
