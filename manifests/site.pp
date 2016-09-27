3d76526bfe34c7c16ca61a3a5f4453c17dd2301a
tischulte <tim.schulte@comcast.net>
2016-09-27 15:00:05 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,11 +38,11 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+#node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
+ # notify { "Hello, my name is ${::hostname}": }
   
-include skeleton
+
 }
