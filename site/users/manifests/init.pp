5f210a2daa98a2d1090a7ed270bb6be46e7aa925
tischulte <tim.schulte@comcast.net>
2016-09-27 13:57:36 -0500

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,4 +1,9 @@
 user {'fundamentals':
   ensure => present,
-  commit => 'test',
   }
+  node default {
+  # This is where you can declare classes for all nodes.
+  # Example:
+  #   class { 'my_class': }
+  notify { "Hello Puppet Master": }
+}
