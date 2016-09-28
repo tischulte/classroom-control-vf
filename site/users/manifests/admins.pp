c861fc84b0c8fd1212a919761936a78c8702f5f1
tischulte <tim.schulte@comcast.net>
2016-09-28 11:07:35 -0500

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -3,7 +3,7 @@ users::managed_user { 'joe': }
 users::managed_user { 'alice':
 group => 'staff',
 }
-users::managed_user { 'aaron':
+users::managed_user { 'chen':
 group => 'staff',
 }
 group { 'staff':
