da95eeaf57b772f1afd3b773856602ad3a5cf923
tischulte <tim.schulte@comcast.net>
2016-09-28 16:51:51 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,30 +1,43 @@
-class nginx {
+class nginx (
+$root = undef,
+) {
 case $::osfamily {
 'redhat','debian' : {
 $package = 'nginx'
 $owner = 'root'
 $group = 'root'
-$docroot = '/var/www'
+# $docroot = '/var/www'
 $confdir = '/etc/nginx'
 $logdir = '/var/log/nginx'
+# this will be used if we don't pass in a value
+$default_docroot = '/var/www'
 }
 'windows' : {
 $package = 'nginx-service'
 $owner = 'Administrator'
 $group = 'Administrators'
-$docroot = 'C:/ProgramData/nginx/html'
+# $docroot = 'C:/ProgramData/nginx/html'
 $confdir = 'C:/ProgramData/nginx'
 $logdir = 'C:/ProgramData/nginx/logs'
+# this will be used if we don't pass in a value
+$default_docroot = 'C:/ProgramData/nginx/html'
 }
 default : {
 fail("Module ${module_name} is not supported on ${::osfamily}")
 }
 }
+# Set the default port for default.conf.epp template
+$port = '80'
 # user the service will run as. Used in the nginx.conf.epp template
 $user = $::osfamily ? {
 'redhat' => 'nginx',
 'debian' => 'www-data',
 'windows' => 'nobody',
+}
+# if $root isn't set, then fall back to the platform default
+$docroot = $root ? {
+undef => $default_docroot,
+default => $root,
 }
 File {
 owner => $owner,
@@ -55,6 +68,7 @@ file { "${confdir}/conf.d/default.conf":
 ensure => file,
 content => epp('nginx/default.conf.epp',
 {
+port => $port,
 docroot => $docroot,
 }),
 notify => Service['nginx'],
