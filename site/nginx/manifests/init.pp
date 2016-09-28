fa9be8748c964a59c8992d027519fc5b10a94207
tischulte <tim.schulte@comcast.net>
2016-09-28 16:39:16 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,28 +1,59 @@
-class nginx (
-$root = undef,
-) {
+class nginx {
 case $::osfamily {
 'redhat','debian' : {
 $package = 'nginx'
 $owner = 'root'
 $group = 'root'
-# $docroot = '/var/www'
+$docroot = '/var/www'
 $confdir = '/etc/nginx'
 $logdir = '/var/log/nginx'
-# this will be used if we don't pass in a value
-$default_docroot = '/var/www'
 }
 'windows' : {
 $package = 'nginx-service'
 $owner = 'Administrator'
 $group = 'Administrators'
-# $docroot = 'C:/ProgramData/nginx/html'
+$docroot = 'C:/ProgramData/nginx/html'
 $confdir = 'C:/ProgramData/nginx'
 $logdir = 'C:/ProgramData/nginx/logs'
-# this will be used if we don't pass in a value
-$default_docroot = 'C:/ProgramData/nginx/html'
 }
 default : {
 fail("Module ${module_name} is not supported on ${::osfamily}")
 }
-
+}
+# user the service will run as. Used in the nginx.conf.epp template
+$user = $::osfamily ? {
+'redhat' => 'nginx',
+'debian' => 'www-data',
+'windows' => 'nobody',
+}
+File {
+owner => $owner,
+group => $group,
+mode => '0664',
+}
+package { $package:
+ensure => present,
+}
+file { [ $docroot, "${confdir}/conf.d" ]:
+ensure => directory,
+}
+file { "${docroot}/index.html":
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+file { "${confdir}/nginx.conf":
+ensure => file,
+content => epp('nginx/nginx.conf.epp',
+{
+user => $user,
+confdir => $confdir,
+logdir => $logdir,
+}),
+notify => Service['nginx'],
+}
+file { "${confdir}/conf.d/default.conf":
+ensure => file,
+content => epp('nginx/default.conf.epp',
+{
+docroot => $docroot,
+}),
