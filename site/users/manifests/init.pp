77c0b435634026c551dd57a68248012772582176
tischulte <tim.schulte@comcast.net>
2016-09-27 21:45:19 -0500

    Create init.pp

---
+++ site/users/manifests/init.pp
@@ -0,0 +1,7 @@
+class users {
+user { 'fundamentals':
+ensure => present,
+# password => 'puppet8#labs', # Windows requires a plain text password
+# groups => ['Users'], # Display in Windows Control Panel
+}
+}
