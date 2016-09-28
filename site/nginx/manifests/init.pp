226338afc0f6b94401b61b2dead1d4bbc1d0a329
tischulte <tim.schulte@comcast.net>
2016-09-28 08:50:57 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,46 +1,39 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+}
+
 package { 'nginx':
-ensure => present,
+  ensure => present,
 }
-file { '/var/www':
-ensure => directory,
-owner => 'root',
-group => 'root',
-mode => '0775',
+
+file { [ '/var/www', '/etc/nginx/conf.d' ]:
+  ensure => directory,
 }
+
 file { '/var/www/index.html':
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',
-source => 'puppet:///modules/nginx/index.html',
+  ensure => file,
+  source => 'puppet:///modules/nginx/index.html',
 }
+
 file { '/etc/nginx/nginx.conf':
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',
-source => 'puppet:///modules/nginx/nginx.conf',
-require => Package['nginx'],
-notify => Service['nginx'],
+  ensure => file,
+  source => 'puppet:///modules/nginx/nginx.conf',
+  require => Package['nginx'],
+  notify => Service['nginx'],
 }
-file { '/etc/nginx/conf.d':
-ensure => directory,
-owner => 'root',
-group => 'root',
-mode => '0775',
-}
+
 file { '/etc/nginx/conf.d/default.conf':
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0664',
-source => 'puppet:///modules/nginx/default.conf',
-require => Package['nginx'],
-notify => Service['nginx'],
+  ensure => file,
+  source => 'puppet:///modules/nginx/default.conf',
+  notify => Service['nginx'],
+  require => Package['nginx'],
 }
+
 service { 'nginx':
-ensure => running,
-enable => true,
+  ensure => running,
+  enable => true,
 }
 }
