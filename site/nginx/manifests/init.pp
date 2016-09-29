142dbb93e951d94fe1be8d826acdf87453730d9d
tischulte <tim.schulte@comcast.net>
2016-09-29 09:42:40 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,44 +1,13 @@
 class nginx (
-$root = undef,
-) {
-case $::osfamily {
-'redhat','debian' : {
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-# $docroot = '/var/www'
-$confdir = '/etc/nginx'
-$logdir = '/var/log/nginx'
-# this will be used if we don't pass in a value
-$default_docroot = '/var/www'
-}
-'windows' : {
-$package = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-# $docroot = 'C:/ProgramData/nginx/html'
-$confdir = 'C:/ProgramData/nginx'
-$logdir = 'C:/ProgramData/nginx/logs'
-# this will be used if we don't pass in a value
-$default_docroot = 'C:/ProgramData/nginx/html'
-}
-default : {
-fail("Module ${module_name} is not supported on ${::osfamily}")
-}
-}
-# Set the default port for default.conf.epp template
-$port = '80'
-# user the service will run as. Used in the nginx.conf.epp template
-$user = $::osfamily ? {
-'redhat' => 'nginx',
-'debian' => 'www-data',
-'windows' => 'nobody',
-}
-# if $root isn't set, then fall back to the platform default
-$docroot = $root ? {
-undef => $default_docroot,
-default => $root,
-}
+$package = $nginx::params::package,
+$owner = $nginx::params::owner,
+$group = $nginx::params::group,
+$docroot = $nginx::params::docroot,
+$confdir = $nginx::params::confdir,
+$logdir = $nginx::params::logdir,
+$user = $nginx::params::user,
+$port = $nginx::params::port
+) inherits nginx::params {
 File {
 owner => $owner,
 group => $group,
@@ -73,8 +42,8 @@ docroot => $docroot,
 }),
 notify => Service['nginx'],
 }
+
 service { 'nginx':
 ensure => running,
 enable => true,
-}
 }
